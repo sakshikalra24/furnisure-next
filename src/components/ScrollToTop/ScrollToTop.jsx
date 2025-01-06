0e5712a4ffacbe44c69d9d5747ef42d600d1fbfa
sk24tw <sakshikalra@thoughtwin.com>
2025-01-06 23:47:07 +0530

    added the navigation

--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const ScrollToTop = () => {
   const router = useRouter();
