8427008f11d42fc5d3cbf1b814f119d0bdfaf5fa
sk24tw <sakshikalra@thoughtwin.com>
2025-01-18 07:02:46 -0600

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,51 +1,48 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import Head from "next/head";
+  import { Geist, Geist_Mono } from "next/font/google";
+  import Head from "next/head";
 
-import AnalyticsTracker from "../components/AnalyticsTracker";
+  import AnalyticsTracker from "../components/AnalyticsTracker";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+  });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+  });
 
-export const metadata = {
-  title: "FurniSure Rentals",
-};
+  export const metadata = {
+    title: "FurniSure Rentals",
+  };
 
-export default function RootLayout({ children }) {
-
-  return (
-    <html lang="en">
-      <Head>
-        {/* Meta tags */}
-        <meta name="theme-color" content="#ffffff" />
-
-        {/* Google Analytics Script */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0HMGVSYBXD"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-0HMGVSYBXD');
-            `,
-          }}
-        />
-      </Head>
-
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        id="root"
-      >
-        <AnalyticsTracker />
-        {children}
-      </body>
-    </html>
-  );
-}
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-0HMGVSYBXD"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-0HMGVSYBXD');
+              `,
+            }}
+          />
+        </head>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          id="root"
+        >
+          <AnalyticsTracker />
+          {children}
+        </body>
+      </html>
+    );
+  }
