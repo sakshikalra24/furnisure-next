370c477b848c05b06cf208fbdb8ae69ead290f1c
sk24tw <sakshikalra@thoughtwin.com>
2025-01-18 06:48:15 -0600

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,36 +1,51 @@
-// app/layout.js (Client-Side)
+import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
 
-import AnalyticsTracker from "../components/AnalyticsTracker"; // Import the Analytics Tracker
+import AnalyticsTracker from "../components/AnalyticsTracker";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "FurniSure Rentals",
+};
 
 export default function RootLayout({ children }) {
+
   return (
-    <>
-      <html lang="en">
-        <head>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-0HMGVSYBXD"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+    <html lang="en">
+      <Head>
+        {/* Meta tags */}
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Google Analytics Script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0HMGVSYBXD"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-0HMGVSYBXD');
             `,
-            }}
-          />
-        </head>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-          id="root"
-        >
-          {/* Include the AnalyticsTracker to track page views on route change */}
-          <AnalyticsTracker />
-          {children}
-        </body>
-      </html>
-    </>
+          }}
+        />
+      </Head>
+
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        id="root"
+      >
+        <AnalyticsTracker />
+        {children}
+      </body>
+    </html>
   );
 }
