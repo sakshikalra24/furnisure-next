071acc7568fdc558a30cf0bafca9c12f6b61e39d
sk24tw <sakshikalra@thoughtwin.com>
2025-02-16 05:26:07 -0600

    made some chnages in the conifg

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   reactStrictMode: false,
   images: {
@@ -12,5 +13,19 @@ export default {
       process.env.NODE_ENV === "production"
         ? "https://furnisure.me/api/woocommerce"
         : "http://localhost:3000/api/woocommerce",
+  },
+  webpackDevMiddleware: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.watchOptions = {
+        ignored: "**/*",
+      };
+    }
+    return config;
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = { fs: false };
+    }
+    return config;
   },
 };
